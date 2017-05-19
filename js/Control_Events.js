0df9d3aa373b726c47ef5776779f0a5eaa37ca40
Epi <e.avila@adissinformatica.com>
2017-05-19 14:19:23 +0200

    v5 19-5-17 14:19

--- js/Control_Events.js
+++ js/Control_Events.js
@@ -40,37 +40,27 @@ function onclick_registrarUSuario() {
     let dato9 = 0;
     let dato10 = 0;
     let dato11 = userimg;
-    // console.log("hola");
-    // console.log(dato8);
-    // console.log("Creando usuario")
-    // console.log(dato8);
 
-    // console.log('imprime banner');
-    // console.log(banner);
-
-    // debugger;
-    // debugger;
-    // console.log(dato11[0].name);
-    // let bannerruta = dato11[0].name;
     let campos = { username : dato1,pass : dato2,email: dato3, name:dato4, surname:dato5,city:dato6,country:dato7,banner:dato8,likes:dato9,views:dato10,userimg:dato11};
-    // campos = [dato1,dato2,dato3,dato4,dato5,dato6,dato7,dato8.name,dato9,dato10,dato11.name];
 
     let data = {metodo : 'crearUsuario',datos : campos};
 
     // console.log(dato8.valor);
     // console.log(dato11.valor);
-
-    // $("#").val();
+    console.log("caracteres:"+banner.length);
     $.post(
         CONEXIONES,
         data,
         function(data){
-            if(data){
+            if(data==true){
+                // let str = data.replace("\/","/");
+                // console.log(str);
+                console.log(data);
                 console.log("Usuario Registrado correctamente")
-                alert("Usuario Registrado correctamente");
+                // alert("Usuario Registrado correctamente");
             }else{
                 console.log("El usuario no se ha podido registrar");
-                alert("El usuario no se ha podido registrar")
+                // alert("El usuario no se ha podido registrar")
 
             }
         }
--- php/CONEXIONES.php
+++ php/CONEXIONES.php
@@ -81,16 +81,20 @@ function crearUsuario(){
 
     $conn = mysqli_connect("localhost","root","","cgplus");
     if(mysqli_connect_errno($conn)){
-        return utf8_encode("Imposible conectarse con la base de datos");
+        return json_encode("Imposible conectarse con la base de datos");
     }else{
 
 
-
-
+//        $banner = addslashes($_POST['banner']);
+//        $banner = $_POST['banner'];
+//        return json_encode($banner);
+//        if($_FILES['banner']['tmp_img']){
+//            return json_encode("Hay archivo");
+//        }
 
         $query = "INSERT INTO users(username, pass, email, name, surname, city, country, bannerimg, likes, views, userimg) VALUES ('".$userdatos['username']."','".$userdatos['pass']."','".$userdatos['email']."','".$userdatos['name']."','".$userdatos['surname']."','".$userdatos['city']."','".$userdatos['country']."','".$userdatos['banner']."',".$userdatos['likes'].",".$userdatos['views'].",'".$userdatos['userimg']."')";
 //        $query = "INSERT INTO users(username, pass, email, name, surname, city, country, bannerimg, likes, views, userimg) VALUES ('usu1','usu1','usu1','usu1','usu1','usu1','usu1','$bannerimg',0,0,'$userimg')";
-//        return json_encode($query);
+//        return json_encode($query);       return json_encode($query);
 
         $result = mysqli_query($conn,$query);
 
@@ -115,21 +119,20 @@ function cargarUsuario(){
 
 
 
-        $query = "SELECT * FROM users where username ='Ambylise'";
+        $query = "SELECT * FROM users where username ='qwerty'";
 //        return json_encode($query);
 //        $query = "INSERT INTO users(username, pass, email, name, surname, city, country, bannerimg, likes, views, userimg) VALUES ('usu1','usu1','usu1','usu1','usu1','usu1','usu1','$bannerimg',0,0,'$userimg')";
 //        return json_encode($query);
 
 
         $result = mysqli_query($conn,$query);
-
+        $arrayUsers = [];
             if($result){
                 $usuario = new Usuario();
 
                 while($linea = mysqli_fetch_assoc($result)){
 
-    //                $usuario = new Usuario($linea['likes'],$linea['username'],$linea['pass'],$linea['email'],$linea['name'],$linea['surname'],$linea['city'],$linea['country'],$linea['bannerimg'],$linea['likes'],$linea['views'],$linea['userimg']);
-
+                    $usuario = new Usuario();
                     $usuario->__set("likes",$linea['likes']);
                     $usuario->__set("username",$linea['username']);
                     $usuario->__set("pass",$linea['pass']);
@@ -143,11 +146,12 @@ function cargarUsuario(){
                     $usuario->__set("views",$linea['views']);
                     $usuario->__set("userimg",$linea['userimg']);
 
+                    array_push($arrayUsers,$usuario);
                 }
                 mysqli_close($conn);
 
 //                return json_encode($usuario);
-                return json_encode($usuario->userimg);
+                return json_encode($arrayUsers);
             }else{
                 mysqli_close($conn);
                 return  json_encode(false);
